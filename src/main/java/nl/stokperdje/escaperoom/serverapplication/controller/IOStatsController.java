ae347794a966fb8bff844758b1271eb08eb3c186
Remco de Hoon <remcodehoon@gmail.com>
2019-12-14 22:44:03 +0100

    Enable IOStats

--- src/main/java/nl/stokperdje/escaperoom/serverapplication/controller/IOStatsController.java
+++ src/main/java/nl/stokperdje/escaperoom/serverapplication/controller/IOStatsController.java
@@ -1,17 +1,22 @@
 package nl.stokperdje.escaperoom.serverapplication.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import nl.stokperdje.escaperoom.serverapplication.dto.IOStats;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping(value = "/iostats")
 public class IOStatsController {
+
+    private Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 
     @PostMapping(consumes = "application/json")
     public ResponseEntity postIOStats(@RequestBody IOStats ioStats) {
