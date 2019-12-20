efe9656e641b806972b2c66151000f88103733d3
Remco de Hoon <remcodehoon@gmail.com>
2019-12-20 15:58:08 +0100

    Added GET for IOStats

--- src/main/java/nl/stokperdje/escaperoom/serverapplication/controller/IOStatsController.java
+++ src/main/java/nl/stokperdje/escaperoom/serverapplication/controller/IOStatsController.java
@@ -7,9 +7,11 @@ import nl.stokperdje.escaperoom.serverapplication.service.WebSocketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping(value = "/iostats")
@@ -20,6 +22,8 @@ public class IOStatsController {
 
     @Autowired
     private SessionService service;
+
+    private RestTemplate restTemplate = new RestTemplate();
 
     @PostMapping(consumes = "application/json")
     public ResponseEntity postIOStats(@RequestBody IOStats ioStats) {
@@ -38,4 +42,18 @@ public class IOStatsController {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @GetMapping
+    public ResponseEntity getIOStats() {
+        try {
+            // IOStats ophalen
+            String url = "http://192.168.2.223:8082/iostats";
+            return new ResponseEntity<>(restTemplate.getForEntity(url, String.class), HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+    }
+
 }
